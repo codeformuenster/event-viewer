7ebcf1bd74821d97aca8a095de0af21d62c9ba1a
Yannic Schencking <y.schencking@gruenspar.com>
2017-08-08 21:36:18 +0200

    Add map to card

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM node:8.1-alpine
 
-# RUN npm install -g create-react-app
+#RUN npm install -g create-react-app
+RUN yarn global add create-react-app
 
 WORKDIR /app
 
 COPY package.json /app
 
+RUN npm install --global
 RUN yarn install
 
 CMD ["yarn", "start"]
--- src/EventCard.js
+++ src/EventCard.js
@@ -1,22 +1,35 @@
+import {} from 'dotenv/config';
 import React, { Component } from 'react';
 import {Card, CardHeader, CardText} from 'material-ui/Card';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import Pin from './Pin';
+
 
 class EventCard extends Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props);
 
     this.state = {
-      items: []
-    }
+      items: [],
+        viewport: {
+        zoom: 15,
+        bearing: 0,
+        pitch: 0,
+        width: 400,
+        height: 400,
+      }
+    };
   }
 
   render() {
+    const {viewport} = this.state;
+
     return (
       <div className="Event">
       {this.state.items.map ((e, index) => {
         return (
-          <div>
+         <div>
             <Card className="Card">
               <CardHeader
                 key={index}
@@ -34,6 +47,23 @@ class EventCard extends Component {
                 <div className="Url">
                   Weitere Informationen unter {e.url}
                 </div>
+                {(e.location.geo) &&
+                 <ReactMapGL
+                    {...viewport}
+                    mapStyle="mapbox://styles/mapbox/dark-v9"
+                    onViewportChange={this._updateViewport}
+                    latitude={e.location.geo.latitude}
+                    longitude={e.location.geo.longitude}
+                    width={400}
+                    height={400}
+
+                    // Optionally call `setState` and use the state to update the map.
+                    >
+                      <Marker latitude={e.location.geo.latitude} longitude={e.location.geo.longitude} offsetLeft={-20} offsetTop={-10}>
+                            <Pin size={30} />
+                        </Marker>
+                    </ReactMapGL>
+                  }
               </CardText>
            </Card>
           </div>
@@ -46,7 +76,28 @@ class EventCard extends Component {
   componentDidMount() {
     fetch('https://event-api.codeformuenster.org/v0/events')
       .then(result => result.json())
-      .then(items => this.setState({items}))
+      .then(items => this.setState({items}));
+
+   window.addEventListener('resize', this._resize);
+    this._resize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this._resize);
+  }
+
+  _resize = () => {
+    this.setState({
+      viewport: {
+        ...this.state.viewport,
+        width: this.props.width || window.innerWidth,
+        height: this.props.height || window.innerHeight
+      }
+    });
+  };
+
+  _updateViewport = (viewport) => {
+    this.setState({viewport});
   }
 }
 
